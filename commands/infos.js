3210c6fc4010459e67799b857d9f3bf796b4d3be
Guillaume Delor <guillaume.delor1@gmail.com>
2020-07-04 12:55:52 +0200

    V0.1.2#Alpha - Ajout help

--- commands/infos.js
+++ commands/infos.js
@@ -1,21 +1,22 @@
 module.exports = {
     name: 'infos',
-    description: 'Permet d\'obtenir des informations sur un élément (server, me, user)',
+    description: 'Permet d\'obtenir des informations sur un élément.',
     args: 1,
+    usage: '<element> (server, me, user) <arg_facultatif> (tag)',
     execute(message, args) {
         switch (args[0]) {
             case 'server':
                 const guilde = message.guild;
-                return message.channel.send(`Serveur : ${guilde.name}\nTotal membres : ${guilde.memberCount}`);
+                return message.channel.send(`**Serveur** : ${guilde.name}\n**Total membres** : ${guilde.memberCount}`);
             case 'me':
                 const auteur = message.author;
-                return message.channel.send(`Membre : ${auteur.username}\nID : ${auteur.id}`);
+                return message.channel.send(`**Membre** : ${auteur.username}\n**ID** : ${auteur.id}`);
             case 'user':
                 if (!args[1]) {
                     return message.reply('Vous devez taguer un membre !');
                 } else {
                     const user = message.mentions.users.first();
-                    return message.channel.send(`Membre : ${user.username}\nID : ${user.id}`);
+                    return message.channel.send(`**Membre** : ${user.username}\n**ID** : ${user.id}`);
                 }
             default:
                 return message.reply('Argument incorrect !');
