3017956bf6b60cc9d3c6aa9f564f56ec849a3892
Guillaume Delor <guillaume.delor1@gmail.com>
2020-07-13 17:00:28 +0200

    V0.2#Beta - Commande membres en request API site

--- commands/membres.js
+++ commands/membres.js
@@ -5,47 +5,80 @@ const {
     version
 } = require(`${appRoot}/config/config.json`);
 
+const axios = require('axios');
+const config = {
+    method: 'get',
+    url: 'http://krisegis.fr/wp-json/krisegis/v1/membres',
+    headers: {}
+};
+
+
 module.exports = {
     name: 'membres',
     aliases: ['membres', 'membresliste', 'krisegiens'],
     description: 'Liste des membres de la Compagnie de Krisegis',
     execute(message, args) {
-        // inside a command, event listener, etc.
-        const exampleEmbed = new Discord.MessageEmbed()
-            .setColor('YELLOW')
-            .attachFiles([`${appRoot}/assets/krisegis-point.png`, `${appRoot}/assets/krisegis.png`])
-            .setTitle('Liste des membres')
-            .setURL('https://krisegis.fr/type_fiche/membre/')
-            .setAuthor('Membres de la Compagnie de Krisegis', 'attachment://krisegis-point.png')
-            .setDescription('Il existe actuellement 13 membres dans la Compagnie de Krisegis.')
-            .addFields({
-                name: 'Dirigeants (2)',
-                value: 'Tahroy\nAluchtal',
-                inline: true,
-            }, {
-                name: 'Experts (1)',
-                value: 'Raven',
-                inline: true
-            }, {
-                name: 'Confirmés (0)',
-                value: '...',
-                inline: true
-            }, {
-                name: 'Apprentis (6)',
-                value: 'Ash\nAspaco\nJasina\nLinley\nWaf\nZaelya',
-                inline: true
-            }, {
-                name: 'Recrues (5)',
-                value: 'Hytori\nMaelys\nMela\nThiva\nYunah-Marre',
-                inline: true
-            }, {
-                name: '\u200B',
-                value: '\u200B',
-                inline: true
-            })
-            .setTimestamp()
-            .setFooter(`Krisegis V${version}`, 'attachment://krisegis.png');
 
-        message.channel.send(exampleEmbed);
+        axios(config)
+            .then(function (response) {
+                const membres = (response.data);
+                var rangs = {
+                    'Dirigeant': [],
+                    'Expert': [],
+                    'Confirmé': [],
+                    'Apprenti': [],
+                    'Recrue': [],
+                };
+
+                for (const membre of membres) {
+                    rangs[membre.rang].push(membre.name);
+                }
+
+                for (const rang in rangs) {
+                    rangs[rang].push('\u200b');
+                }
+
+                // inside a command, event listener, etc.
+                const exampleEmbed = new Discord.MessageEmbed()
+                    .setColor('YELLOW')
+                    .attachFiles([`${appRoot}/assets/krisegis-point.png`, `${appRoot}/assets/krisegis.png`])
+                    .setTitle('Liste des membres')
+                    .setURL('https://krisegis.fr/type_fiche/membre/')
+                    .setAuthor('Membres de la Compagnie de Krisegis', 'attachment://krisegis-point.png')
+                    .setDescription(`Il existe actuellement ${membres.length} membres dans la Compagnie de Krisegis.`)
+                    .addFields({
+                        name: `Dirigeants (${rangs.Dirigeant.length - 1})`,
+                        value: `${rangs.Dirigeant.join('\n')}`,
+                        inline: true,
+                    }, {
+                        name: `Experts (${rangs.Expert.length - 1})`,
+                        value: `${rangs.Expert.join('\n')}`,
+                        inline: true,
+                    }, {
+                        name: `Confirmés (${rangs.Confirmé.length - 1})`,
+                        value: `${rangs.Confirmé.join('\n')}`,
+                        inline: true,
+                    }, {
+                        name: `Apprentis (${rangs.Apprenti.length - 1})`,
+                        value: `${rangs.Apprenti.join('\n')}`,
+                        inline: true,
+                    }, {
+                        name: `Recrues (${rangs.Recrue.length - 1})`,
+                        value: `${rangs.Recrue.join('\n')}`,
+                        inline: true,
+                    }, {
+                        name: '\u200B',
+                        value: '\u200B',
+                        inline: true
+                    })
+                    .setTimestamp()
+                    .setFooter(`Krisegis V${version}`, 'attachment://krisegis.png');
+
+                message.channel.send(exampleEmbed);
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     },
 };
