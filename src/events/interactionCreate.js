a3c1401aadf7aba72bfc2f2b2a0e8ac1c6a908c1
Guillaume Delor <guillaume.delor1@gmail.com>
2023-02-05 18:04:58 +0100

    Fix error

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -12,7 +12,11 @@ module.exports = function (client) {
             return await command.execute(interaction);
         } catch (error) {
             console.error(error);
-            return await interaction.reply({content: `Erreur ${error.message}`, ephemeral: true});
+            try {
+                return await interaction.reply({content: `Erreur ${error.message}`, ephemeral: true});
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
     this.gererBouton = async function (interaction)
