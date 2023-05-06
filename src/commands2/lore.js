0b61fad291c31a6a8c9313867c2197009c0dcd73
Tahroy <guillaume.delor1@gmail.com>
2023-05-06 20:54:41 +0200

    Ajout pagination pour commande lore

--- src/commands2/lore.js
+++ src/commands2/lore.js
@@ -1,11 +1,21 @@
-const { SlashCommandBuilder } = require('discord.js')
+const { SlashCommandBuilder, ButtonBuilder, ActionRowBuilder } = require('discord.js')
 const { api_lore } = require('../../config/config.json')
 const axios = require('axios')
-const { PermissionFlagsBits } = require('discord-api-types/v8')
+const { PermissionFlagsBits, ButtonStyle } = require('discord-api-types/v8')
 const { escapeHTML, substringContent } = require('../utils/Utils')
 const embedData = require('../utils/embed')
 
 const WIKI_RP = 'https://dofus-rp.fandom.com/fr/'
+
+let interactionsCache = [];
+
+const NOMS = {
+    item: 'Objets',
+    npc: 'PNJ',
+    document: 'Documents',
+    article: 'Articles',
+    page: "Pages WIKI"
+};
 
 module.exports = {
     opts: {},
@@ -16,6 +26,7 @@ module.exports = {
             .setDescription(
                 'recherche sur les objets, documents, articles ou dialogues de PNJ')
             .setRequired(true)),
+    interactions: [],
     async execute (interaction) {
 
         const search = encodeURI(interaction.options.getString('search'))
@@ -23,35 +34,70 @@ module.exports = {
         await interaction.reply('Voici ce que j\'ai !')
 
         await console.log(`Recherche de ${search} par ${interaction.user.username}`)
-        const response = await axios.get(api_lore + '?content=' + `"${search}"`)
 
-        console.log(api_lore + '?content=' + `"${search}"`)
+        const tab = [
+            { type: 'item', truncate: true },
+            { type: 'npc', truncate: true },
+            { type: 'document', truncate: true },
+            { type: 'article', truncate: false }
+        ];
 
-        const data = response.data.data
-        await this.sendResults(interaction, data.items, 'objet')
-        await this.sendResults(interaction, data.npcs, 'PNJ')
-        await this.sendResults(interaction, data.documents, 'document')
-        await this.sendResults(interaction, data.articles, 'article', false)
+        for (const item of tab) {
+            const data = await this.getData(item.type, search)
+            const send = await this.sendResults(interaction, data, item.type, item.truncate)
+            this.saveInteraction(send, data, item.type, search);
+        }
 
         try {
-            const wikiResponse = await axios.get(WIKI_RP + `api.php?action=query&list=search&srsearch=${search}&format=json`)
+            const callWiki = WIKI_RP + `api.php?action=query&list=search&srsearch=${search}&format=json`;
+
+            const wikiResponse = await axios.get(callWiki);
 
             const WikiData = wikiResponse.data.query.search
 
             let pages = []
             for (let i = 0; i < WikiData.length; i++) {
                 const item = WikiData[i]
-                pages.push({ name: item.title, content: [WIKI_RP + `wiki/?curid=${item.pageid}`] })
+                pages.push({
+                    name: item.title,
+                    content: [WIKI_RP + `wiki/?curid=${item.pageid}`]
+                })
             }
-            await this.sendResults(interaction, pages, 'page', false)
+
+            let data = {
+                'data': pages,
+                'limit': 10,
+                'offset': 0,
+                'total': pages.length,
+            };
+
+            await this.sendResults(interaction, data, 'page', false)
         } catch (error) {
             console.error(error)
             await interaction.channel.send('Erreur lors de la récupération des données WIKI')
         }
     },
 
-    async sendResults (interaction, items = [], name = '', truncate = true) {
+    async getData (route, search, offset = 0) {
+        const LIMIT = 20;
+        const call = `${api_lore}/${route}?content="${search}"&$limit=${LIMIT}&$offset=${offset}`
+        console.log(call)
+        const response = await axios.get(call)
+        return response.data
+    },
+
+    async getResult (data, type = '', truncate = true) {
+        const items = data.data
+        const limit = parseInt(data.limit)
+        const offset = parseInt(data.offset)
+        const total = parseInt(data.total)
+
+        const nombrePages = Math.ceil(total / limit) ? Math.ceil(total / limit) : 1
+        const pageActuelle = Math.ceil(offset / limit) + 1
+
         let lines = []
+        let categorie = NOMS[type];
+
         for (let i = 0; i < items.length; i++) {
             const item = items[i]
             const name = escapeHTML(item.name)
@@ -64,23 +110,84 @@ module.exports = {
             }
 
             lines.push(`- **${name}** : ${content}`)
-
-            if (i === 20) {
-                lines.push(`*Certains résultats ont été masqués, précisez la requête*`)
-                i = items.length
-            }
         }
 
-        if (lines.length > 1) {
-            name += 's'
-        }
+        lines.push(`\nPage ${pageActuelle}/${nombrePages}`)
 
         let itemsEmbed = embedData.createEmbed([], {
-            title: `**${items.length} ${name} :**`,
+            title: `**${total} ${categorie} :**`,
             description: lines.join('\n'),
-            author: 'Recherche : ' + interaction.options.getString('search')
         })
 
-        await interaction.channel.send({ embeds: itemsEmbed.embeds, files: itemsEmbed.files })
+        const precedent = new ButtonBuilder()
+            .setCustomId(`lore-last`)
+            .setLabel('Précedent')
+            .setStyle(ButtonStyle.Success)
+            .setDisabled(offset === 0)
+
+        const suivant = new ButtonBuilder()
+            .setCustomId(`lore-next`)
+            .setLabel('Suivant')
+            .setStyle(ButtonStyle.Success)
+            .setDisabled(offset + limit >= total)
+
+        const row = new ActionRowBuilder()
+        row.addComponents(precedent)
+        row.addComponents(suivant)
+
+        return {
+            embeds: itemsEmbed.embeds,
+            files: itemsEmbed.files,
+            components: [row],
+        }
+    },
+
+    async executeButton (interaction, buttonName) {
+        const interactionCache = interactionsCache[interaction.message.id];
+        const message = interaction.message;
+
+        if (!interactionCache) {
+            interaction.channel.send('Erreur lors de la récupération des données de la requête.')
+        }
+
+        let offset = parseInt(interactionCache.offset);
+
+        if (buttonName === 'last') {
+            offset -= interactionCache.limit;
+        }
+        else if (buttonName === 'next') {
+            offset += interactionCache.limit;
+        }
+
+        const data = await this.getData(interactionCache.type, interactionCache.search, offset);
+        await this.editResults(interactionCache.message, data, interactionCache.type, interactionCache.type !== 'article');
+        this.saveInteraction(message, data, interactionCache.type, interactionCache.search);
+
+        return await interaction.deferUpdate();
+    },
+    saveInteraction (message, data, name = '', search = '') {
+        const type = name
+        const limit = parseInt(data.limit)
+        const offset = parseInt(data.offset)
+        const total = parseInt(data.total)
+
+        interactionsCache[message.id] = {
+            message: message,
+            search: search,
+            type: type,
+            limit: limit,
+            offset: offset,
+            total: total
+        }
+    },
+
+    async sendResults (interaction, data, type = '', truncate = true) {
+        const retour = await this.getResult(data, type, truncate)
+        return await interaction.channel.send(retour)
+    },
+
+    async editResults (message, data, type, truncate) {
+        const retour = await this.getResult(data, type, truncate);
+        return await message.edit(retour);
     }
 }
