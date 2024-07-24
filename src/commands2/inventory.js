892c123e3154b363fdfd1040bc6cf00ee3d21c63
Tahroy <guillaume.delor1@gmail.com>
2024-07-24 15:04:06 +0200

    Ajout de l'inventaire

---
+++ src/commands2/inventory.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('discord.js')
+const embedData = require('../utils/embed')
+const PlayerItem = require('../database/PlayerItem')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('inventory')
+        .setDescription('Permet de consulter son inventaire')
+    ,
+    async execute (interaction) {
+        const user = interaction.user
+        const member = interaction.member
+        const memberName = member.nickname ?? member.user.globalName
+
+        const playerItems = await PlayerItem.findAll({
+            where: {
+                user_id: user.id
+            },
+            order: [['name', 'ASC']]
+        })
+
+        if (playerItems.length === 0) {
+            await interaction.reply('Votre inventaire est vide')
+            return
+        }
+
+        let items = [];
+        for (const playerItem of playerItems) {
+            items.push(`${playerItem.get('quantity') } x ${playerItem.get('name')}`)
+        }
+
+        const embed = embedData.createEmbed([], {
+            title: `Inventaire de ${memberName}`,
+            description: items.join('\n')
+        })
+
+        await interaction.reply({ embeds: embed.embeds, files: embed.files })
+    },
+}
