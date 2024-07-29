133bdd9fa6feda20ea92a8535f9958f7f573b36b
Tahroy <guillaume.delor1@gmail.com>
2024-07-29 16:25:37 +0200

    ajout log sur les serveurs discord

--- src/events/ready.js
+++ src/events/ready.js
@@ -57,9 +57,18 @@ module.exports = async function (client) {
             console.error,
         )
 
-        client.guilds.cache.forEach(guild => {
+        for (const guild of client.guilds.cache) {
             console.log(`- ${guild.name} (ID: ${guild.id})`)
-        })
+
+            if (guild.id != '185464480346537984') {
+                // Liste des membres du serveur
+                await guild.members.fetch() // Récupère les membres du serveur
+                console.log(`  Membres de ${guild.name}:`)
+                guild.members.cache.forEach(member => {
+                    console.log(`  - ${member.user.tag} (ID: ${member.id})`)
+                })
+            }
+        }
 
 
         checkEventReminders()
