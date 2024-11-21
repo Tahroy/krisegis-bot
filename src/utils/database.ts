f5d65965fa2c87d76e5176d2f86ab82f743f7846
Tahroy <guillaume.delor1@gmail.com>
2024-11-21 01:17:08 +0100

    JS to TS

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
+import path from "path";
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: './../database.sqlite',
+    storage: path.join(__dirname, './../../database.sqlite'), // Utilisation de __dirname pour construire un chemin absolu
     logging: false, // Désactiver les logs de Sequelize
 });
 
