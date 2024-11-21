7fb2de944d4c5d4a30f5fdabbd47023d9e2503ad
Tahroy <guillaume.delor1@gmail.com>
2024-11-21 01:19:40 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Récupérez les dernières modifications
+git pull origin main
+
+# Installez les dépendances
+sudo npm install
+
+# Compilez l'application
+npm run build
+
+# Redémarrez l'application avec PM2
+pm2 restart dist/index.js
+
+echo "Déploiement terminé avec succès!"
