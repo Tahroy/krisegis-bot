39b5db97fc40bbd762e56ea0d2160a01c0b973c8
Tahroy <guillaume.delor1@gmail.com>
2025-01-19 02:21:14 +0100

    Ajout de lore à Astrub Economie

--- src/commands/astrub_economy/Information.ts
+++ src/commands/astrub_economy/Information.ts
@@ -19,7 +19,7 @@ De partout récolteurs et artisans trouvent ici une nouvelle vie et un travail, 
 
 Vous allez donc devoir travailler à votre tour pour gagner votre pain et nous aider à faire prosperer la ville.
 
-Pour cela, rien de plus simple ! Prenez un métier et commencer à récolter. Vous pouvez vendre directement au marché vos ressources, mais il y a encore mieux !
+Pour cela, rien de plus simple ! Prenez un métier et commencez à récolter. Vous pouvez vendre directement au marché vos ressources, mais il y a encore mieux !
 Si vous avez assez de kamas en poche ou que vous connaissez un bon bûcheron ou mineur, vous pouvez utiliser des outils, comme une marmite ou un four pour fabriquer d'autres produits bien plus rentables !
 
 Attention cependant, la météo a tendance à être Tumultueuse par ici. On dit qu'une déesse rieuse s'intéresse à nous...
