938c8f1d6b7c0dbd0097ba94062e296923579517
Tahroy <guillaume.delor1@gmail.com>
2025-01-21 01:24:13 +0100

    début housing

--- src/models/astrub_economy/Ressource.ts
+++ src/models/astrub_economy/Ressource.ts
@@ -5,7 +5,7 @@ import {JobEnum, RessourcesEnum} from "./Enums";
 interface Ressource extends BaseItem{
     sell: number;   // Prix de vente
     buy: number;    // Prix d'achat
-    job: string;
+    job?: string;
     level: number;
 }
 
@@ -17,6 +17,9 @@ const Ressources: Record<RessourcesEnum, Ressource> = {
     [RessourcesEnum.GOUJON]: {name: RessourcesEnum.GOUJON, job: JobEnum.PECHEUR, level: 1, sell: 2, buy: 5, type: ItemType.RESSOURCE, emoji: "goujon"},
     [RessourcesEnum.FRENE]: {name: RessourcesEnum.FRENE, job: JobEnum.BUCHERON, level: 1, sell: 2, buy: 5, type: ItemType.RESSOURCE, emoji: "frene"},
     [RessourcesEnum.FER]: {name: RessourcesEnum.FER, job: JobEnum.MINEUR, level: 1, sell: 2, buy: 5, type: ItemType.RESSOURCE, emoji: "fer"},
+    [RessourcesEnum.KAMAS]: {name: RessourcesEnum.KAMAS, job: undefined, level: 1, sell: 0, buy: 0, type: ItemType.RESSOURCE, emoji: "kamas"},
+    [RessourcesEnum.PIERRE]: {name: RessourcesEnum.PIERRE, job:undefined, level:1, sell:0, buy: 3, type: ItemType.RESSOURCE, emoji: "pierre"},
+    [RessourcesEnum.LAINE_DE_BOUFTOU]: {name: RessourcesEnum.LAINE_DE_BOUFTOU, job:undefined, level:1, sell:0, buy: 3, type: ItemType.RESSOURCE, emoji: "laine"}
 };
 
 export default Ressource;
