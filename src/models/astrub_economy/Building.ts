90bac58c97952c93176bf679413c1a6372ea398e
Tahroy <guillaume.delor1@gmail.com>
2025-01-27 00:02:47 +0100

    update Building.ts

--- src/models/astrub_economy/Building.ts
+++ src/models/astrub_economy/Building.ts
@@ -18,10 +18,10 @@ const Buildings: Record<BuildingEnum, Building> = {
         name: BuildingEnum.SANCTUAIRE,
         description: "Petit sanctuaire fait de pierre et de bois disposé au centre du marché. Les habitants peuvent y déposer une offrande dans la coupe pour espérer que les Dix leur répondent.",
         recipe: {
-            [RessourcesEnum.PIERRE]: 600,
-            [CraftEnum.PLANCHE]: 300,
-            [CraftEnum.BARRE_DE_FER]: 300,
-            [RessourcesEnum.KAMAS]: 5000
+            [RessourcesEnum.PIERRE]: 3500,
+            [CraftEnum.PLANCHE]: 1000,
+            [CraftEnum.BARRE_DE_FER]: 1000,
+            [RessourcesEnum.KAMAS]: 10000
         },
         image: 'sanctuaire.png'
     },
@@ -29,10 +29,10 @@ const Buildings: Record<BuildingEnum, Building> = {
         name: BuildingEnum.SCIERIE,
         description: "Scierie fabriquée en bois avec une scie en fer. Les bûcherons peuvent y découper leur bois pour créer des planches.",
         recipe: {
-            [RessourcesEnum.FRENE]: 2000,
-            [RessourcesEnum.FER]: 800,
-            [RessourcesEnum.PIERRE]: 800,
-            [RessourcesEnum.KAMAS]: 5000
+            [RessourcesEnum.FRENE]: 5000,
+            [RessourcesEnum.FER]: 2500,
+            [RessourcesEnum.PIERRE]: 2500,
+            [RessourcesEnum.KAMAS]: 10000
         },
         image: 'scierie.png'
     },
@@ -40,10 +40,10 @@ const Buildings: Record<BuildingEnum, Building> = {
         name: BuildingEnum.FORGE,
         description: "Forge fabriquée en pierre avec plusieurs belles enclumes. Les mineurs peuvent y fabriquer des barres de fer.",
         recipe: {
-            [RessourcesEnum.FRENE]: 800,
-            [RessourcesEnum.FER]: 2000,
-            [RessourcesEnum.PIERRE]: 800,
-            [RessourcesEnum.KAMAS]: 5000
+            [RessourcesEnum.FRENE]: 2500,
+            [RessourcesEnum.FER]: 5000,
+            [RessourcesEnum.PIERRE]: 2500,
+            [RessourcesEnum.KAMAS]: 10000
         },
         image: 'forge.png'
     }
