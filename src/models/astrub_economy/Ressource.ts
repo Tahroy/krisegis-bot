7e2d1a156318e5383258a962c90206a39cc72aaa
Tahroy <guillaume.delor1@gmail.com>
2025-05-03 19:39:02 +0200

    Ajout commentaires

--- src/models/astrub_economy/Ressource.ts
+++ src/models/astrub_economy/Ressource.ts
@@ -19,7 +19,7 @@ const Ressources: Record<RessourcesEnum, Ressource> = {
     [RessourcesEnum.FRENE]: {name: RessourcesEnum.FRENE, job: JobEnum.BUCHERON, level: 0, sell: SellEnum.RESSOURCE_1, buy: BuyEnum.RESSOURCE_1, type: ItemType.RESSOURCE, emoji: "frene"},
     [RessourcesEnum.FER]: {name: RessourcesEnum.FER, job: JobEnum.MINEUR, level: 0, sell: SellEnum.RESSOURCE_1, buy: BuyEnum.RESSOURCE_1, type: ItemType.RESSOURCE, emoji: "fer"},
 
-    // Nouvelles ressources de niveau 10
+    // Niveau 10
     [RessourcesEnum.CHATAIGNIER]: {name: RessourcesEnum.CHATAIGNIER, job: JobEnum.BUCHERON, level: 10, sell: SellEnum.RESSOURCE_10, buy: BuyEnum.RESSOURCE_10, type: ItemType.RESSOURCE, emoji: "chataignier"},
     [RessourcesEnum.CUIVRE]: {name: RessourcesEnum.CUIVRE, job: JobEnum.MINEUR, level: 10, sell: SellEnum.RESSOURCE_10, buy: BuyEnum.RESSOURCE_10, type: ItemType.RESSOURCE, emoji: "cuivre"},
     [RessourcesEnum.SAUGE]: {name: RessourcesEnum.SAUGE, job: JobEnum.ALCHIMISTE, level: 10, sell: SellEnum.RESSOURCE_10, buy: BuyEnum.RESSOURCE_10, type: ItemType.RESSOURCE, emoji: "sauge"},
@@ -27,12 +27,13 @@ const Ressources: Record<RessourcesEnum, Ressource> = {
     [RessourcesEnum.GREUVETTE]: {name: RessourcesEnum.GREUVETTE, job: JobEnum.PECHEUR, level: 10, sell: SellEnum.RESSOURCE_10, buy: BuyEnum.RESSOURCE_10, type: ItemType.RESSOURCE, emoji: "greuvette"},
     [RessourcesEnum.TRUITE]: {name: RessourcesEnum.TRUITE, job: JobEnum.PECHEUR, level: 10, sell: SellEnum.RESSOURCE_10, buy: BuyEnum.RESSOURCE_10, type: ItemType.RESSOURCE, emoji: "truite"},
 
+    // Ressources neutres
     [RessourcesEnum.KAMAS]: {name: RessourcesEnum.KAMAS, job: undefined, level: 0, sell: 0, buy: 0, type: ItemType.RESSOURCE, emoji: "kamas"},
 
     [RessourcesEnum.PIERRE]: {name: RessourcesEnum.PIERRE, job:undefined, level:0, sell:0, buy: 3, type: ItemType.RESSOURCE, emoji: "pierre"},
     [RessourcesEnum.LAINE_DE_BOUFTOU]: {name: RessourcesEnum.LAINE_DE_BOUFTOU, job:undefined, level:0, sell:0, buy: 0, type: ItemType.RESSOURCE, emoji: "laine"},
-    [RessourcesEnum.CUIR]: {name: RessourcesEnum.CUIR, job:undefined, level:15, sell:0, buy: 15, type: ItemType.RESSOURCE, emoji: "cuir"},
-    [RessourcesEnum.TISSU]: {name: RessourcesEnum.TISSU, job:undefined, level:15, sell:0, buy: 15, type: ItemType.RESSOURCE, emoji: "tissu"}
+    [RessourcesEnum.CUIR]: {name: RessourcesEnum.CUIR, job:undefined, level:0, sell:0, buy: 15, type: ItemType.RESSOURCE, emoji: "cuir"},
+    [RessourcesEnum.TISSU]: {name: RessourcesEnum.TISSU, job:undefined, level:0, sell:0, buy: 15, type: ItemType.RESSOURCE, emoji: "tissu"}
 };
 
 export default Ressource;
