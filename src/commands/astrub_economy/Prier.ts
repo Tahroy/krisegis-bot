1540e9fed561bb6eed58a1a74c592e5bec880a4b
Tahroy <guillaume.delor1@gmail.com>
2025-07-23 00:21:35 +0200

    Fix pierre + debug + nouvelle prière

--- src/commands/astrub_economy/Prier.ts
+++ src/commands/astrub_economy/Prier.ts
@@ -1,19 +1,18 @@
-import {CommandInteraction, MessageFlags, SlashCommandBuilder} from "discord.js";
+import {ChatInputCommandInteraction, CommandInteraction, MessageFlags, SlashCommandBuilder} from "discord.js";
 import PlayerItem from "../../models/PlayerItem";
 import JobUtil from "../../services/JobUtil";
 import {Meteos} from "../../models/astrub_economy/Meteo";
 import AbstractSubCommand from "../../utils/AbstractSubCommand";
 import {SlashCommandSubcommandBuilder} from "@discordjs/builders";
+import {QuestService} from "../../services/questService";
+import KrisegisClient from "../../models/KrisegisClient";
 
 export class Prier extends AbstractSubCommand {
     name = 'prier'
     description = 'Prier les dieux au sanctuaire'
+    private static readonly OPTION_PRIERE = 'priere';
 
-    async execute(interaction: CommandInteraction): Promise<void> {
-        if (!interaction.isChatInputCommand()) {
-            return
-        }
-
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
         const guildId = interaction.guild?.id;
         if (!guildId) {
             await interaction.reply({
@@ -23,15 +22,38 @@ export class Prier extends AbstractSubCommand {
             return;
         }
 
-        const prayer = interaction.options.getString('prayer')
+        const prayer = interaction.options.getString(Prier.OPTION_PRIERE)
 
-        if (prayer !== 'meteo') {
-            await interaction.reply({
-                content: 'Priere inconnue',
-                flags: MessageFlags.Ephemeral
-            })
-            return
+        switch (prayer) {
+            case 'meteo':
+                await this.executeMeteo(interaction, guildId)
+                return;
+            case 'quete':
+                await this.executeQuete(interaction, guildId)
+                return;
+            default:
+                await interaction.reply({
+                    content: 'Priere inconnue',
+                    flags: MessageFlags.Ephemeral
+                })
+                return
         }
+    }
+
+    protected addOptions(builder: SlashCommandSubcommandBuilder) {
+        builder.addStringOption(
+            option => option
+                .setName(Prier.OPTION_PRIERE)
+                .setDescription('Prière de votre choix')
+                .setRequired(true)
+                .setChoices(
+                    {name: 'Changement de météo (500 kamas)', value: 'meteo'},
+                    {name: 'Déclenche une quête (500 kamas)', value: 'quete'}
+                )
+        )
+    }
+
+    private async executeMeteo(interaction: CommandInteraction, guildId: string) {
 
         // On vérifie que la personne a le bon nombre de kamas
         const user = interaction.user;
@@ -78,14 +100,51 @@ export class Prier extends AbstractSubCommand {
         }
     }
 
-    protected addOptions(builder: SlashCommandSubcommandBuilder) {
-        builder.addStringOption(
-            option => option
-                .setName('prayer')
-                .setDescription('Prière de votre choix')
-                .setRequired(true)
-                .setChoices({name: 'Changement de météo (500 kamas)', value: 'meteo'},)
-        )
-    }
+    private async executeQuete(interaction: ChatInputCommandInteraction, guildId: string) {
+        // On vérifie que la personne a le bon nombre de kamas
+        const user = interaction.user;
+        const kamas = await PlayerItem.findOne({
+            where: {
+                name: 'Kamas',
+                userId: user.id,
+                guildId: guildId
+            }
+        })
 
+        if (!kamas || kamas.quantity < 500) {
+            await interaction.reply({
+                content: "Vous n'avez pas assez de kamas",
+                flags: MessageFlags.Ephemeral
+            })
+            return
+        }
+
+        const quest = await QuestService.generateRandomQuest(guildId);
+
+        if (!quest) {
+            await interaction.reply({
+                content: "Impossible de générer une nouvelle quête !",
+                flags: MessageFlags.Ephemeral
+            })
+            return
+        }
+
+        await QuestService.announceQuest(interaction.client as KrisegisClient, quest);
+
+        // On retire les kamas
+        await PlayerItem.update({quantity: kamas.quantity - 500}, {
+            where: {
+                name: 'Kamas',
+                userId: user.id,
+                guildId: guildId
+            }
+        })
+
+        await interaction.reply({
+            content: 'Les dieux ont entendu...',
+            flags: MessageFlags.Ephemeral
+        })
+
+
+    }
 }
