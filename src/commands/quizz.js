bf2c96de79c2804927f0e0c423e70e409310e64a
Tahroy <31081296+Tahroy@users.noreply.github.com>
2025-08-02 12:02:56 +0200

    useless code

--- src/commands/quizz.js
+++ src/commands/quizz.js
@@ -5,7 +5,6 @@ const Question = require('../models/Question').default
 const { createEmbed } = require('../utils/embed')
 const { PlayerService, ItemType } = require('../services/PlayerService')
 
-// Utilisez un objet pour stocker les scores des participants par canal
 const channelScores = new Map()
 
 const adminsIds = [
@@ -130,13 +129,11 @@ module.exports = {
 
         currentQuestion.participations.set(interaction.user.id, selectedAnswer)
 
-        // Incrémentez le score du participant
         const authorId = interaction.user.id
         if (!currentChannelScores.has(authorId)) {
             currentChannelScores.set(authorId, 0)
         }
 
-        // Vérifiez si la réponse est correcte
         if (currentQuestion.correctAnswer === selectedAnswer) {
             currentChannelScores.set(authorId, currentChannelScores.get(authorId) + 1)
             await PlayerService.addPlayerItem(
@@ -148,7 +145,6 @@ module.exports = {
             )
         }
 
-        // Passez à la question suivante
         console.log(`${interaction.user.username} a répondu ${selectedAnswer}`)
         await interaction.reply({ 'content': `Tu as répondu « ${selectedAnswer} »`, 'ephemeral': true })
     },
@@ -156,7 +152,6 @@ module.exports = {
 
         const nombre = interaction.options.getInteger('nb_questions')
 
-        // Créez un canal de scores pour le canal actuel
         if (!channelScores.has(interaction.channelId)) {
             const currentChannelData = {
                 scores: new Map(),
@@ -164,7 +159,6 @@ module.exports = {
                 questions: [],
             }
 
-            // Ajout de 10 questions aléatoires (sans répétition) dans le canal
             const availableQuestions = await getRandomQuestions()
             //console.log(availableQuestions)
             for (let i = 0; i < nombre; i++) {
@@ -178,12 +172,10 @@ module.exports = {
             channelScores.set(interaction.channelId, currentChannelData)
         }
 
-        // Commencez le quizz en envoyant la première question
         sendQuestion(interaction)
         interaction.reply({ 'content': 'Le quizz a commencé !', 'ephemeral': true })
     },
     addQuestion (interaction) {
-        // Si ce n'est pas un admin, on retourne une erreur
         if (!adminsIds.includes(interaction.user.id)) {
             return interaction.reply({ 'content': 'Vous n\'avez pas la permission de faire cela !', 'ephemeral': true })
         }
@@ -332,7 +324,7 @@ function sendQuestion (interaction) {
     console.log('avant', selectedAnswers)
     const finalAnswers = shuffleArray(selectedAnswers);
     console.log('apres', finalAnswers)
-    // Créez les boutons de réponse
+
     const buttons = finalAnswers.map((answer) => {
         return new ButtonBuilder()
             .setCustomId(`quizz-${answer}`)
@@ -340,10 +332,8 @@ function sendQuestion (interaction) {
             .setStyle(ButtonStyle.Primary)
     })
 
-    // Créez une rangée de boutons
     const row = new ActionRowBuilder().addComponents(buttons)
 
-    // Envoyez la question avec les boutons
     interaction.channel.send({
         content: currentQuestion.question,
         components: [row],
@@ -351,15 +341,12 @@ function sendQuestion (interaction) {
 
 //    console.log(`Question envoyée ${currentQuestion.question}`)
 
-    // Définissez un délai de 30 secondes pour répondre à la question
     setTimeout(async () => {
         await interaction.channel.send(`Le temps est écoulé ! La réponse était « **${currentQuestion.correctAnswer}** »`)
-        // Passez à la question suivante
         currentChannelData.currentQuestionIndex++
         if (currentChannelData.currentQuestionIndex < currentChannelData.questions.length) {
             sendQuestion(interaction)
         } else {
-            // Le quizz est terminé, annoncez le score
             const currentChannelData = channelScores.get(interaction.channelId)
             const currentChannelScores = currentChannelData.scores ?? new Map()
             let scoreMessage = `Quizz terminé ! Scores :\n`
@@ -374,7 +361,6 @@ function sendQuestion (interaction) {
     }, 15000)
 }
 
-// Fonction de mélange (shuffle) basée sur l'algorithme de Fisher-Yates
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -392,7 +378,6 @@ async function getRandomQuestions () {
             return []
         }
 
-        // Mélangez les questions de manière aléatoire
         return questions.sort(() => 0.5 - Math.random())
     } catch (error) {
         console.error('Erreur lors de la récupération des questions aléatoires :', error)
