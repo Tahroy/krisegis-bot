dfa240ad73475f56119ed5ee90fc9dbe8016134e
Tahroy <guillaume.delor1@gmail.com>
2025-10-04 22:38:02 +0200

    Equilibre prix bouftou

--- src/services/EconomyService.ts
+++ src/services/EconomyService.ts
@@ -42,7 +42,7 @@ export class EconomyService {
             [ResourceEnum.CUIR]: 6,
             [ResourceEnum.TISSU]: 6,
             [ResourceEnum.OEUF]: 4,
-            [ResourceEnum.BOUFTOU]: 5000
+            [ResourceEnum.BOUFTOU]: 1000
         };
 
         if (item.type === ItemType.RESSOURCE) {
